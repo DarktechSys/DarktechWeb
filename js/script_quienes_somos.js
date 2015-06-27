65e8a3193e533699258d0b775fcb481ad83779cd
jpachecov <la.parte.imaginaria@gmail.com>
2015-06-27 14:38:11 -0500

    carrusel

--- js/script_quienes_somos.js
+++ js/script_quienes_somos.js
@@ -1,76 +1,73 @@
-/* var cualno = 1;
-function ocultaDivs(){
-	$("#quienes_somos_anim1").addClass("invisible");
-	$("#quienes_somos_anim2").addClass("invisible");
-	$("#quienes_somos_anim3").addClass("invisible");
-	//hazAnimacion();	
-};
+var div1 = "\<div class='contenedor' id='quienes_somos_anim1'>\
+				<div class='cuadro_texto'>\
+					<br>\
+					<h3>Salimos del esquema</h3>\
+					<p>Nuestro objetivo siempre es buscar soluciones\
+					innovadoras no importando el problema, adaptando las actuales y nuevas tecnologías para lograrlo.</p>\
+				</div>\
+				<i class='fa fa-unlock fa-5x'></i>\
+				<i class='fa fa-sellsy fa-5x'></i>\
+				<i class='fa fa-rocket fa-5x'></i>\
+				<i class='fa fa-thumbs-o-up fa-5x'></i>\
+				<i class='fa fa-code-fork fa-5x'></i>\
+			</div>";
 
-function hazAnimacion(){
-	hazInvisibles(cualno);
-	$("#quienes_somos_anim"+cualno).toggle("slide",3000);
-	window.setTimeout(function(){$("#quienes_somos_anim"+cualno).toggle("slide",3000)},1000);
-	
-		if(cualno == 3){
-		cualno = 1;
-		return;
+var div2 = "\<div class='contenedor' id='quienes_somos_anim2'>\
+				<div class='cuadro_texto'>\
+					<br>\
+					<h3>El diseño sí importa</h3>\
+					<p>El buen diseño de una interfaz no debe dejarse de lado, de él depende el buen entendimiento y fácil uso de una aplicación, nosotros te ofrecemos eso, diseño pensado en el usuario, pensado para que navegues, trabajes o lo que hagas con la aplicación de forma sencilla y eficaz.</p>\
+				</div>\
+				<i class='fa fa-star fa-5x'></i>\
+				<i class='fa fa-diamond fa-5x'></i>\
+				<i class='fa fa-magic fa-5x'></i>\
+				<i class='fa fa-css3 fa-5x'></i>\
+				<i class='fa fa-html5 fa-5x'></i>\
+			</div>";
+
+var div3 = "\<div class='contenedor' id='quienes_somos_anim3'>\
+				<div class='cuadro_texto'>\
+					<br>\
+					<h3>Te llevamos al futuro</h3>\
+					<p>Las tecnologías informáticas son herramientas, nosotros\
+					te ayudaremos a que las incorpores a tu negocio, automatizando tareas\
+					o simplemente dando a conocer tus ideas con una página web.</p>\
+				</div>\
+				<i class='fa fa-cubes fa-5x'></i>\
+				<i class='fa fa-line-chart fa-5x'></i>\
+				<i class='fa fa-briefcase fa-5x'></i>\
+				<i class='fa fa-globe fa-5x'></i>\
+				<i class='fa fa-lightbulb-o fa-5x'></i>\
+			</div>";
+
+var divs = [div1,div2,div3];
+var n = 0;
+
+function ejecutaCarrusel(){
+	$("#principal > div").remove();
+	if (n == 3) {
+		n = 0;
 	}
-	cualno++;
 	
-};
-function hazInvisibles(cualno){
-	for(i = 1; i < 4; i++){
-		if(i!=cualno){
-			$("#quienes_somos_anim"+i).addClass("invisible");
-		}
-	}
+	$("#principal").append(divs[n]);
+	$("#principal > div").show().fadeOut(2000);	
+	n++;
 }
 
-$(document).ready(ocultaDivs());
-window.setInterval(function(){hazAnimacion()},8000);
-*/
 
-var div1 = '<div class="contenedor" id="quienes_somos_anim1">
-				<div class="cuadro_texto">
-
-					<h3>Salimos del esquema</h3>
-					<p>Nuestro objetivo siempre es buscar soluciones
-					innovadoras no importando el problema, adaptando las actuales y nuevas tecnologías para lograrlo.</p>
-				</div><!--
-				-->
-				<div class="cuadro_anim">
-					<i class="fa fa-connectdevelop fa-5x"></i>
-				</div>
-			</div>';
-
-var div2 = '<div class="contenedor" id="quienes_somos_anim2">
-				<div class="cuadro_texto">
-
-					<h3>El diseño sí importa</h3>
-					<p>El buen diseño de una interfaz no debe dejarse de lado, de él depende el buen entendimiento y fácil uso de una aplicación, nosotros te ofrecemos eso, diseño pensado en el usuario, pensado para que navegues, trabajes o lo que hagas con la aplicación de forma sencilla y eficaz.</p>
-				</div><!--
-				-->
-				<div class="cuadro_anim">
-					<i class="fa fa-star fa-5x"></i>
-				</div>
-
-			</div>';
-			
-var div3 = '<div class="contenedor" id="quienes_somos_anim3">
-				<div class="cuadro_texto">
-
-					<h3>Te llevamos al futuro</h3>
-					<p>Las tecnologías informáticas son herramientas, nosotros
-					te ayudaremos a que las incorpores a tu negocio, automatizando tareas
-					o simplemente dando a conocer tus ideas con una página web.</p>
-				</div>
-				<div class="cuadro_anim">
-					<i class="fa fa-rocket fa-5x"></i>
-				</div>
-			</div>
-		</div>';
+var idCarrusel;
+$(document).ready(idCarrusel = window.setInterval(ejecutaCarrusel,2000));
+//$(document).ready($("#principal").append(div1));
 
 
+$("#principal").mouseover(function(){
+	clearInterval(idCarrusel);
+	$("#principal > div").remove();
+	if(n != 0){
+		$("#principal").append(divs[n-1]).show();	
+	}
+});
 
-
-
+$("#principal").mouseout(function(){
+	idCarrusel = window.setInterval(ejecutaCarrusel,2000);
+});
