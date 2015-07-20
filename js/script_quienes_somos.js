7786d7c9fc58dc21b5543eefc1f1fbb4c28ed6eb
jpachecov <la.parte.imaginaria@gmail.com>
2015-07-20 13:38:51 -0500

    quienes somos

--- js/script_quienes_somos.js
+++ js/script_quienes_somos.js
@@ -1,33 +1,46 @@
 var div1 = "\<div class='contenedor' id='quienes_somos_anim1'>\
+				<div id='iconos_izquierda'>\
+					<i class='fa fa-unlock fa-5x'></i>\
+					<i class='fa fa-sellsy fa-5x'></i>\
+					<i class='fa fa-rocket fa-5x'></i>\
+				</div>\
 				<div class='cuadro_texto'>\
 					<br>\
 					<h3>Salimos del esquema</h3>\
 					<p>Nuestro objetivo siempre es buscar soluciones\
 					innovadoras no importando el problema, adaptando las actuales y nuevas tecnologías para lograrlo.</p>\
 				</div>\
-				<i class='fa fa-unlock fa-5x'></i>\
-				<i class='fa fa-sellsy fa-5x'></i>\
-				<i class='fa fa-rocket fa-5x'></i>\
-				<i class='fa fa-thumbs-o-up fa-5x'></i>\
-				<i class='fa fa-code-fork fa-5x'></i>\
-				<i class='fa fa-puzzle-piece fa-5x'></i>\
+				<div id='iconos_derecha'>\
+					<i class='fa fa-thumbs-o-up fa-5x'></i>\
+					<i class='fa fa-code-fork fa-5x'></i>\
+					<i class='fa fa-puzzle-piece fa-5x'></i>\
+				</div>\
 			</div>";
-
+//Juv93D6vp0
 var div2 = "\<div class='contenedor' id='quienes_somos_anim2'>\
+				<div id='iconos_izquierda'>\
+					<i class='fa fa-star fa-5x'></i>\
+					<i class='fa fa-diamond fa-5x'></i>\
+					<i class='fa fa-magic fa-5x'></i>\
+				</div>\
 				<div class='cuadro_texto'>\
 					<br>\
 					<h3>El diseño sí importa</h3>\
 					<p>El buen diseño de una interfaz no debe dejarse de lado, de él depende el buen entendimiento y fácil uso de una aplicación, nosotros te ofrecemos eso, diseño pensado en el usuario, pensado para que navegues, trabajes o lo que hagas con la aplicación de forma sencilla y eficaz.</p>\
 				</div>\
-				<i class='fa fa-star fa-5x'></i>\
-				<i class='fa fa-diamond fa-5x'></i>\
-				<i class='fa fa-magic fa-5x'></i>\
-				<i class='fa fa-css3 fa-5x'></i>\
-				<i class='fa fa-html5 fa-5x'></i>\
-				<i class='fa fa-heart fa-5x'></i>\
+				<div id='iconos_derecha'>\
+					<i class='fa fa-css3 fa-5x'></i>\
+					<i class='fa fa-html5 fa-5x'></i>\
+					<i class='fa fa-heart fa-5x'></i>\
+				</div>\
 			</div>";
 
 var div3 = "\<div class='contenedor' id='quienes_somos_anim3'>\
+				<div id='iconos_izquierda'>\
+					<i class='fa fa-cubes fa-5x'></i>\
+					<i class='fa fa-line-chart fa-5x'></i>\
+					<i class='fa fa-briefcase fa-5x'></i>\
+				</div>\
 				<div class='cuadro_texto'>\
 					<br>\
 					<h3>Te llevamos al futuro</h3>\
@@ -35,12 +48,11 @@ var div3 = "\<div class='contenedor' id='quienes_somos_anim3'>\
 					te ayudaremos a que las incorpores a tu negocio, automatizando tareas\
 					o simplemente dando a conocer tus ideas con una página web.</p>\
 				</div>\
-				<i class='fa fa-cubes fa-5x'></i>\
-				<i class='fa fa-line-chart fa-5x'></i>\
-				<i class='fa fa-briefcase fa-5x'></i>\
-				<i class='fa fa-globe fa-5x'></i>\
-				<i class='fa fa-lightbulb-o fa-5x'></i>\
-				<i class='fa fa-user-plus fa-5x'></i>\
+				<div id='iconos_derecha'>\
+					<i class='fa fa-globe fa-5x'></i>\
+					<i class='fa fa-lightbulb-o fa-5x'></i>\
+					<i class='fa fa-user-plus fa-5x'></i>\
+				</div>\
 			</div>";
 
 var divs = [div1,div2,div3];
